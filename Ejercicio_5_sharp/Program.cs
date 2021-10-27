c2a16f57991a619d59aef7526da880506fa3b01a
Meliodas-R <inceptiongodmaxwell@gmail.com>
2021-10-27 13:47:59 +0200

    Agregada la operación para realizar la media y su muestreo por pantalla.

--- Ejercicio_5_sharp/Program.cs
+++ Ejercicio_5_sharp/Program.cs
@@ -6,7 +6,21 @@ namespace Ejercicio_5_sharp
     {
         static void Main(string[] args)
         {
-            
+            // Declaracion de variables.
+            int contador = 1;
+            decimal media = 0;
+
+            // Pedimos al usuario que introduzca números mientras se cumpla la condición.
+            do
+            {
+                Console.Write("Introduzca un número: ");
+                media = media + Convert.ToDecimal(Console.ReadLine());
+                contador++;
+            }
+
+            while (contador <= 3);
+            media = Math.Round((media / 3), 2);
+            Console.WriteLine("\nLa media es: " + media);
         }
     }
 }
